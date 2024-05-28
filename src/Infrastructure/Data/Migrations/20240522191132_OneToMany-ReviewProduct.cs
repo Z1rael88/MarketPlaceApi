d6fc37bad0b10bda40231f2936c9317cb60d3373
Z1rael88 <oleksiibahmet@gmail.com>
2024-05-28 13:18:01 +0200

    Unversioned migration files

---
+++ src/Infrastructure/Data/Migrations/20240522191132_OneToMany-ReviewProduct.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPlaceApi.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneToManyReviewProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewText",
+                table: "Reviews",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Reviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ProductId",
+                table: "Reviews",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Products_ProductId",
+                table: "Reviews",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Products_ProductId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_ProductId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewText",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
